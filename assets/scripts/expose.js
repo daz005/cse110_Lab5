159365d39d84ef7ba2477ef4128b7a5824fe5662
Your Name <your_email@whatever.com>
2022-10-29 16:59:28 -0700

    on_volume_changed

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,7 +8,43 @@ function init() {
   document.getElementById("horn-select").addEventListener("change", on_horn_select_changed)
   {
 
+  };
+
+  document.getElementById("volume").addEventListener("change", on_volum_changed)
+  {
+
   }
+
+}
+function on_volum_changed() {
+  //console.log("on_volum_changed");
+  var x = document.getElementById("volume").value;
+  //console.log(x);
+  
+  const nodeList = document.getElementById("volume-controls").childNodes
+  for (let i = 0; i < nodeList.length; i++) 
+  {
+    if (nodeList[i].nodeName == "IMG")
+    {
+      if(x == 0)
+      { 
+          nodeList[i].src="assets/icons/volume-level-0.svg";
+      }
+      else if( x < 33 )
+      {
+        nodeList[i].src="assets/icons/volume-level-1.svg";
+      }
+      else if(x < 67 )
+      {
+        nodeList[i].src="assets/icons/volume-level-2.svg";
+      }else
+      {
+        nodeList[i].src="assets/icons/volume-level-3.svg";
+      }
+    }
+  }
+
+
 }
 
 function on_horn_select_changed() {
